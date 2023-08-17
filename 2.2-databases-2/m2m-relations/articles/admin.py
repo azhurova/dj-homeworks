2c3d9697dd9a2b28db31bed247230b6f1c6e2908
azhurova <zhurovaa@gmail.com>
2023-08-17 16:28:28 +0300

    m2m-relations

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,36 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Tag, Scope
+
+
+class ScoupeInlineFormset(BaseInlineFormSet):
+    def save(self):
+        count_is_main = 0
+        for form in self.forms:
+            if form.cleaned_data['is_main']:
+                count_is_main += 1
+
+        if count_is_main != 1:
+            raise ValidationError('Один из тегов должен быть основным')
+        return super().save()  # вызываем базовый код переопределяемого метода
+
+
+class ScoupeInline(admin.TabularInline):
+    model = Scope
+    extra = 0
+    formset = ScoupeInlineFormset
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    inlines = [
+        ScoupeInline,
+    ]
+    list_display = ['title', 'text', 'published_at', 'image']
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name']
--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -1,16 +1,39 @@
 from django.db import models
 
+BOOL_CHOICES = ((True, 'Да'), (False, 'Нет'))
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=256, verbose_name='Раздел')
+
+    class Meta:
+        verbose_name = 'Раздел'
+        verbose_name_plural = 'Разделы'
+
+    def __str__(self):
+        return self.name
+
 
 class Article(models.Model):
-
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
-    image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    image = models.ImageField(null=True, blank=True, verbose_name='Изображение', )
 
     class Meta:
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
+        ordering = ['-published_at']
 
     def __str__(self):
         return self.title
+
+
+class Scope(models.Model):
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='scopes', verbose_name='Раздел')
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes', verbose_name='Статья')
+    is_main = models.BooleanField(choices=BOOL_CHOICES, verbose_name='Основной')
+
+    class Meta:
+        verbose_name = 'Раздел'
+        verbose_name_plural = 'Разделы'
