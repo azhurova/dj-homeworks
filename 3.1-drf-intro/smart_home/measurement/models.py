78d749a499a2825d14590c8724fb03b570538142
azhurova <zhurovaa@gmail.com>
2023-08-19 23:50:04 +0300

    smart_home image

--- 3.1-drf-intro/smart_home/measurement/models.py
+++ 3.1-drf-intro/smart_home/measurement/models.py
@@ -11,6 +11,7 @@ class Measurement(models.Model):
     sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE, related_name='measurements')
     temperature = models.DecimalField(decimal_places=2, max_digits=6)
     created_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(null=True)
 
     class Meta:
         ordering = ['-created_at']
--- 3.1-drf-intro/smart_home/measurement/serializers.py
+++ 3.1-drf-intro/smart_home/measurement/serializers.py
@@ -1,18 +1,30 @@
 from rest_framework import serializers
+from drf_extra_fields.fields import Base64ImageField
 
 from measurement.models import Measurement, Sensor
 
 
 class MeasurementPostSerializer(serializers.ModelSerializer):
+    image = Base64ImageField(max_length=None, use_url=False, required=False)
+
     class Meta:
         model = Measurement
-        fields = ['sensor', 'temperature', 'created_at']
+        fields = ['sensor', 'temperature', 'created_at', 'image']
+
+    def get_photo_url(self, obj):
+        request = self.context.get('request')
+        photo_url = obj.fingerprint.url
+        return request.build_absolute_uri(photo_url)
+
+    def get_validation_exclusions(self):
+        exclusions = super(MeasurementPostSerializer, self).get_validation_exclusions()
+        return exclusions + ['image']
 
 
 class MeasurementSerializer(serializers.ModelSerializer):
     class Meta:
         model = Measurement
-        fields = ['temperature', 'created_at']
+        fields = ['temperature', 'created_at', 'image']
 
 
 class SensorSerializer(serializers.ModelSerializer):
