433a0b88568154e01805b39af3a7315911041462
azhurova <zhurovaa@gmail.com>
2023-08-21 21:55:36 +0300

    api_with_restrictions

--- 3.3-permissions/api_with_restrictions/advertisements/serializers.py
+++ 3.3-permissions/api_with_restrictions/advertisements/serializers.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
-from advertisements.models import Advertisement
+from advertisements.models import Advertisement, AdvertisementStatusChoices
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -9,8 +10,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'first_name',
-                  'last_name',)
+        fields = ('id', 'username', 'first_name', 'last_name')
 
 
 class AdvertisementSerializer(serializers.ModelSerializer):
@@ -22,8 +22,9 @@ class AdvertisementSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Advertisement
-        fields = ('id', 'title', 'description', 'creator',
-                  'status', 'created_at', )
+        fields = ['id', 'creator', 'created_at',
+                  'status', 'title', 'description']
+        read_only_fields = ['id', 'creator', 'created_at']
 
     def create(self, validated_data):
         """Метод для создания"""
@@ -40,6 +41,18 @@ class AdvertisementSerializer(serializers.ModelSerializer):
     def validate(self, data):
         """Метод для валидации. Вызывается при создании и обновлении."""
 
-        # TODO: добавьте требуемую валидацию
+        user = self.context["request"].user
+
+        open_advertisements = Advertisement.objects.all().filter(creator=user, status=AdvertisementStatusChoices.OPEN)
+        if self.instance:
+            open_advertisements = open_advertisements.exclude(id=self.instance.id)
+            advertisement_status = self.instance.status
+        else:
+            advertisement_status = AdvertisementStatusChoices.OPEN
+        if 'status' in data:
+            advertisement_status = data['status']
+
+        if advertisement_status == AdvertisementStatusChoices.OPEN and open_advertisements.count() >= 10:
+            raise ValidationError("У пользователя может быть не больше 10 открытых объявлений")
 
         return data
--- 3.3-permissions/api_with_restrictions/advertisements/views.py
+++ 3.3-permissions/api_with_restrictions/advertisements/views.py
@@ -1,15 +1,32 @@
+from django_filters import rest_framework
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
+from advertisements.models import Advertisement
+from advertisements.permissions import IsOwner
+from advertisements.serializers import AdvertisementSerializer
+
+
+class AdvertisementFilter(rest_framework.FilterSet):
+    created_at = rest_framework.DateFromToRangeFilter()
+
+    class Meta:
+        model = Advertisement
+        fields = ['title', 'status', 'creator', 'created_at', 'updated_at']
 
 class AdvertisementViewSet(ModelViewSet):
     """ViewSet для объявлений."""
-
-    # TODO: настройте ViewSet, укажите атрибуты для кверисета,
-    #   сериализаторов и фильтров
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    filterset_class = AdvertisementFilter
+    filterset_fields = ['title', 'status', 'creator', 'created_at', 'updated_at']
+    search_fields = ['title', 'description', 'status', 'creator']
+    ordering_fields = ['id', 'title', 'status', 'creator', 'created_at', 'updated_at']
 
     def get_permissions(self):
         """Получение прав для действий."""
-        if self.action in ["create", "update", "partial_update"]:
+        if self.action in ["create"]:
             return [IsAuthenticated()]
+        if self.action in ["update", "partial_update"]:
+            return [IsAuthenticated(), IsOwner()]
         return []
